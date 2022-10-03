f7a7eb0417bd3f651bd8015e48cc1f35dbd06276
Brandon Magofna <magofna68@Brandons-MacBook-Pro.local>
2022-10-03 14:55:29 -0700

    add firebase to application and begin setting up config

---
+++ src/firebase/Firebase.utils.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyBhnVx1XWwHwiosiNmpjDDGwZw8QyKIJUY",
+  authDomain: "m68-e-commerce.firebaseapp.com",
+  projectId: "m68-e-commerce",
+  storageBucket: "m68-e-commerce.appspot.com",
+  messagingSenderId: "756447485486",
+  appId: "1:756447485486:web:6f633390210d031e0409bf"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;

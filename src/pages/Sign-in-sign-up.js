a3184f1b815beef384ffc834e31ea0f0286eeb25
Brandon Magofna <magofna68@Brandons-MacBook-Pro.local>
2022-10-03 13:36:34 -0700

    add sign-in/sign-up page, along with sign-in component w/ handleChange and handleSubmit methods

---
+++ src/pages/Sign-in-sign-up.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './sign-in-sign-up.syles.scss';
+import SignIn from '../sign-in/Sign-in';
+
+const SignInAndSignUpPage = () => (
+  <div className='sign-in-and-sign-up'>
+    <SignIn />
+  </div>
+);
+
+export default SignInAndSignUpPage;
---
+++ src/sign-in/Sign-in.jsx
@@ -0,0 +1,62 @@
+import React, { Component, setState } from 'react'
+import './sign-in.styles.scss';
+
+export default class SignIn extends React.Component {
+constructor(props) {
+  super(props);
+  this.State = {
+    email: "",
+    password: "",
+    
+  }
+}
+
+handleSubmit = (event) => {
+  event.preventDefault();
+  const { email, password } = event.target;
+  console.log({email, password});
+  // this.setState({email: ''}, {password: ''});
+  this.setState({email: "", password: ""});
+  console.log("form submitted", {email, password});
+}
+
+handleChange = (e) => {
+  // e.preventDefault();
+  const { value, name } = e.target;
+  console.log({value, name});
+  // dynamically set property value
+  this.setState({[name]: value})
+}
+
+  render() {
+    return (
+      <div className='sign-in'>
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <label>Email</label>
+          <input 
+            name="email" 
+            type="email" 
+            id="email_input"
+            // value={this.state.email}
+            onChange={this.handleChange}
+            required 
+          />
+          <label>Password</label>
+          <input 
+            name="password" 
+            type="password" 
+            password="password_input"
+            // value={this.state.password} 
+            onChange={this.handleChange}
+            required 
+          />
+        
+          <button type="submit" variant="primary" value="Submit Form">Submit</button>
+        </form>
+      </div>
+    )
+  }
+}

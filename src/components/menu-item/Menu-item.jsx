c9462e63adf1bc5949e6f94435699c074a6ef5a0
Brandon Magofna <magofna68@Brandons-MacBook-Pro.local>
2022-10-10 14:37:06 -0700

    finish building out homepage components and updated styling

--- src/components/menu-item/Menu-item.jsx
+++ src/components/menu-item/Menu-item.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import './menu-item.scss';
 
  const MenuItem = ({ title, imageUrl, size }) => (
-  <div style={{
+  <div className={`${size} menu-item`}>
+  <div 
+  className="background-image"
+  style={{
     backgroundImage: `url(${imageUrl})`,
-  }} className={`${size} menu-item`}>
-    <div className='content'>
-      <h1 className='title'>{title}</h1>
-      <span className='subtitle'>SHOP NOW</span>
-    </div>
+  }}
+  />
+      <div className='content'>
+        <h1 className='title'>{title}</h1>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
   </div>
 )
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 // import Header from './components/common/Header'
-// import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

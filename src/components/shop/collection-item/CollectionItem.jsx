85a9cf91924ab73211d0ad37684e1948bc2a41c8
Brandon Magofna <magofna68@Brandons-MacBook-Pro.local>
2022-10-17 15:04:41 -0700

    :sparkles: update gender components for dynamic views

--- src/components/shop/collection-item/CollectionItem.jsx
+++ src/components/shop/collection-item/CollectionItem.jsx
@@ -1,22 +1,70 @@
 import './collectionItem.styles.scss';
 import React from 'react';
+import Card from 'react-bootstrap/Card'
 
 
 const CollectionItem = ({id, name, price, img, img2}) => (
-  <div className='collectionItem'>
-    <div 
-      className='image'
+  // <div className='collectionItem'>
+  //   <div 
+  //     className='image'
+  //     style={{
+  //       backgroundImage: `url(${img})`,
+  //       onMouseOver: `url(${img2})`
+  //     }}
+  //   />
+  //   <div className='collectionFooter'>
+  //     <span className='name'>{ name }</span>
+  //     <br/>
+  //     <span className='price'>{ price }</span>
+  //   </div>
+  // </div>
+
+  <Card 
+    className='collectionItem'
+    style={{ 
+      display: 'flex',
+      border: 'none',
+      width: '16rem',
+      margin: '10px',
+      height: '375px',
+    }}>
+    <Card.Img 
+      variant='top' 
+      src={img} 
       style={{
-        backgroundImage: `url(${img})`,
-        onMouseOver: `url(${img2})`
+        height: '75%'
+      }}/>
+    <Card.Body 
+      style={{
+        height: '25%',
+        // background: 'orange',
+        display: 'flex',
+        alignContent: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: '3% 2%',
+        textAlign: 'center',
+        flexDirection: 'column',
       }}
-    />
-    <div className='collectionFooter'>
-      <span className='name'>{ name }</span>
-      <br/>
-      <span className='price'>{ price }</span>
-    </div>
-  </div>
+    >
+      <Card.Header 
+        style={{ 
+          background: 'white',
+          borderBottom: 'none'
+        }}
+      >
+        {name}
+      </Card.Header>
+      <Card.Text
+        style={{
+          marginBottom: '3%',
+          fontWeight: '600'
+        }}
+      >
+        {price}
+      </Card.Text>
+    </Card.Body>
+  </Card>
 );
 
 export default CollectionItem;
--- src/components/shop/preview-collection/CollectionPreview.jsx
+++ src/components/shop/preview-collection/CollectionPreview.jsx
@@ -2,25 +2,43 @@ import React from 'react'
 import './collectionPreview.styles.scss'
 import CollectionItem from '../collection-item/CollectionItem';
 import { useNavigate } from 'react-router';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
 
 // const navigate = useNavigate()
 
 export const CollectionPreview = ({title, items, routeName}) => (
-    <div className='collectionPreview'>
-      <h1 
-        className='title'
+    // <div 
+    <Container
+      fluid
+      className='collectionPreview'
+    >
+      <Row>
+        <h1 
+          className='title'
+          >
+          {title.toUpperCase()}
+        </h1>
+      </Row>
+      <div 
+        className='preview'
+        style={{ 
+          flexWrap: 'wrap',
+          display: 'flex',
+          alignContent: 'space-between',
+          justifyContent: 'center',
+        }}
       >
-        {title.toUpperCase()}
-      </h1>
-      <div className='preview'>
         {items
           // limit to only 4 items to display; gets rerendered with component
           .filter((item, index)=> index < 4)
           .map(({id, ...otherItemProps}) => (
             <CollectionItem key={id} {...otherItemProps} />
-        ))}
+            ))}
       </div>
-    </div>
+    {/* </div> */}
+    </Container>
   )
 
   export default CollectionPreview;
